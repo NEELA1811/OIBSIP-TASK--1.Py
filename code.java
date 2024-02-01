8760ca019f83252f23a5eea12ba16419a8512644
NEELA1811 <157453205+NEELA1811@users.noreply.github.com>
2024-02-01 14:12:35 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,1 +1,22 @@
-
+print("            BMI CALCULATOR                    ")
+weight=float(input(" ENTER YOUR WEIGHT IN KILOGRAM: "))
+height=float(input(" ENTER YOUR HEIGHT IN CENTIMETERS:"))
+height_met=height/100
+BMI=weight/(height_met**2)
+print(BMI)
+if(BMI<19):
+    {
+        print("YOU ARE UNDERWEIGHT")
+    }
+elif(BMI>=19 and BMI<=24):
+    {
+        print("YOU ARE HEALTHY!")
+    }
+elif(BMI>=25 and BMI<=30):
+    {
+        print("YOU ARE OVER WEIGHT")
+    }
+elif(BMI>30):
+    {
+        print("YOU ARE OBESE")
+    }
